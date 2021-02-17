73463bd03198405cd623e499ef555bb96bff3ad3
tokineko1348 <78891411+tokineko1348@users.noreply.github.com>
2021-02-17 12:13:53 +0900

    a

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
-
 Bundler.require(*Rails.groups)
 
 module ChatApp
   class Application < Rails::Application
     config.load_defaults 6.0
-    
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
